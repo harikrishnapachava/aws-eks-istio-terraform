c367bf95dce7231855a31bd1e5785fdc6fefbe90
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 16:02:46 +0400

    Update Istiod.tf

--- eks-tf/Istiod.tf
+++ eks-tf/Istiod.tf
@@ -1,15 +1,18 @@
 # helm repo add istio https://istio-release.storage.googleapis.com/charts
 # helm repo update
 # helm install my-istiod-release -n istio-system --create-namespace istio/istiod --set telemetry.enabled=true --set global.istioNamespace=istio-system
+
+# Deploy Istiod (Istio control plane) using Helm
 resource "helm_release" "istiod" {
   name = "my-istiod-release"
 
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "istiod"
-  namespace        = "istio-system"
-  create_namespace = true
-  version          = "1.17.1"
+  repository       = "https://istio-release.storage.googleapis.com/charts" # Helm repository for Istio
+  chart            = "istiod"                                             # Helm chart for Istiod
+  namespace        = "istio-system"                                       # Namespace for Istiod
+  create_namespace = true                                                 # Create the namespace if it doesn't exist
+  version          = "1.17.1"                                             # Version of Istiod
 
+  # Configuration values for Istiod
   set {
     name  = "telemetry.enabled"
     value = "true"
@@ -30,5 +33,6 @@ resource "helm_release" "istiod" {
     value = "gateway"
   }
 
+  # Ensure Istio base is deployed before Istiod
   depends_on = [helm_release.istio_base]
 }
