8d8f21a3b6c5884c2a3367301910710ce84ec6a5
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:58:50 +0400

    Update IAMDriver.tf

--- eks-tf/IAMDriver.tf
+++ eks-tf/IAMDriver.tf
@@ -1,3 +1,4 @@
+# Define an IAM policy document for the EBS CSI driver
 data "aws_iam_policy_document" "csi" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -16,11 +17,13 @@ data "aws_iam_policy_document" "csi" {
   }
 }
 
+# Create an IAM role for the EBS CSI driver
 resource "aws_iam_role" "eks_ebs_csi_driver" {
   assume_role_policy = data.aws_iam_policy_document.csi.json
   name               = "eks-ebs-csi-driver"
 }
 
+# Attach the AmazonEBSCSIDriverPolicy to the IAM role
 resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
   role       = aws_iam_role.eks_ebs_csi_driver.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
