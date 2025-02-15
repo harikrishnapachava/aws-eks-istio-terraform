d80ced978395a0c3d93ace79b5b5c394ae0f49da
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:50:17 +0400

    Update IGW.tf

--- eks-tf/IGW.tf
+++ eks-tf/IGW.tf
@@ -1,6 +1,8 @@
+# Create an Internet Gateway for the VPC
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id # Associate the IGW with the VPC
 
+  # Tags for identifying the Internet Gateway
   tags = {
     Name = "igw"
   }
