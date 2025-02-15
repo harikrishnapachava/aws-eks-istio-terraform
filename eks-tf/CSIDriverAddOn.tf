35e3676e00ad5640701d77e9be04c5e47d668495
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:59:43 +0400

    Update CSIDriverAddOn.tf

--- eks-tf/CSIDriverAddOn.tf
+++ eks-tf/CSIDriverAddOn.tf
@@ -1,6 +1,7 @@
+# Add the EBS CSI driver to the EKS cluster
 resource "aws_eks_addon" "csi_driver" {
-  cluster_name             = aws_eks_cluster.demo.name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.29.1-eksbuild.1"
-  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
+  cluster_name             = aws_eks_cluster.demo.name # Name of the EKS cluster
+  addon_name               = "aws-ebs-csi-driver"     # Name of the add-on
+  addon_version            = "v1.29.1-eksbuild.1"     # Version of the add-on
+  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn # IAM role for the add-on
 }
