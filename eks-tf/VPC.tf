ac489f498fb5ead37d9ad20322a2431552c3887f
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:49:44 +0400

    Update VPC.tf

--- eks-tf/VPC.tf
+++ eks-tf/VPC.tf
@@ -1,10 +1,14 @@
+# Create a VPC with a specified CIDR block
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16" # CIDR block for the VPC
 
+  # Enable DNS support and hostnames for the VPC
   enable_dns_support   = true
   enable_dns_hostnames = true
 
+  # Tags for identifying the VPC
   tags = {
     Name = "main"
   }
 }
+
