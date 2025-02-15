378234f689d9f8eb1ec1492db0ac823cca0a6709
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:48:44 +0400

    Update NormalProvider.tf

--- eks-tf/NormalProvider.tf
+++ eks-tf/NormalProvider.tf
@@ -1,12 +1,14 @@
+# Configure the AWS provider with the specified region
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2" # AWS region where resources will be deployed
 }
 
+# Define Terraform settings, including required providers and their versions
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.56"
+      source  = "hashicorp/aws" # Source of the AWS provider
+      version = "~> 4.56"        # Version constraint for the AWS provider
     }
   }
 }
