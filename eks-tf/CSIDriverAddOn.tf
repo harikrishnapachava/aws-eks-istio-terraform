a8d172e0c79e05969bb1fd5c3876d6402d058575
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 18:06:46 +0400

    Update CSIDriverAddOn.tf

--- eks-tf/CSIDriverAddOn.tf
+++ eks-tf/CSIDriverAddOn.tf
@@ -1,4 +1,11 @@
-# Add the EBS CSI driver to the EKS cluster
+# Installing the EBS CSI Driver in EKS
+# This section installs the AWS EBS CSI driver as an EKS add-on.
+
+# Step 1: Add the EBS CSI Driver to the EKS cluster
+# Deploys the EBS CSI driver as an add-on to the EKS cluster.
+# Uses the IAM role (eks-ebs-csi-driver) to interact with AWS EBS.
+# The driver automates volume provisioning and attachment.
+
 resource "aws_eks_addon" "csi_driver" {
   cluster_name             = aws_eks_cluster.demo.name # Name of the EKS cluster
   addon_name               = "aws-ebs-csi-driver"     # Name of the add-on
