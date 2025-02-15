0a2976bc91268f3b0194850458b1bf1d501128ab
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:53:52 +0400

    Update and rename Routes.tf to Routes_and_RTAssociations.tf

--- eks-tf/Routes_and_RTAssociations.tf
+++ eks-tf/Routes_and_RTAssociations.tf
@@ -1,6 +1,8 @@
+# Create a private route table for private subnets
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
+  # Route for internet access via the NAT Gateway
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
@@ -11,9 +13,11 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Create a public route table for public subnets
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
+  # Route for internet access via the Internet Gateway
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -24,6 +28,7 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Associate private subnets with the private route table
 resource "aws_route_table_association" "private_us_west_2a" {
   subnet_id      = aws_subnet.private_us_west_2a.id
   route_table_id = aws_route_table.private.id
@@ -34,6 +39,7 @@ resource "aws_route_table_association" "private_us_west_2b" {
   route_table_id = aws_route_table.private.id
 }
 
+# Associate public subnets with the public route table
 resource "aws_route_table_association" "public_us_west_2a" {
   subnet_id      = aws_subnet.public_us_west_2a.id
   route_table_id = aws_route_table.public.id
