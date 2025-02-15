22bc96658e5bd1d40e0bc0656aa7f4786138c0e5
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 18:28:03 +0400

    Update Create_IAM_OIDC_Provider.tf

--- eks-tf/Create_IAM_OIDC_Provider.tf
+++ eks-tf/Create_IAM_OIDC_Provider.tf
@@ -1,13 +1,13 @@
 #This section configures OIDC (OpenID Connect) authentication for the EKS cluster. It allows Kubernetes service accounts to assume IAM roles securely.
 
-#Step 1: Retrieve the TLS certificate for the EKS cluster's OIDC provider
-# Retrieves the TLS certificate for the OIDC provider of the EKS cluster.
+#Step 1: Retrieve the TLS certificate for the EKS cluster's OIDC Issuer
+# Retrieves the TLS certificate for the OIDC Issuer of the EKS cluster.
 # The aws_eks_cluster.demo.identity[0].oidc[0].issuer is the OIDC issuer URL of the EKS cluster.
 data "tls_certificate" "eks" {
   url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
 }
 
-# Step 2: Create an IAM OpenID Connect (OIDC) provider for the EKS cluster
+# Step 2: Create an IAM OpenID Connect (OIDC) provider in IAM, and point it to the EKS cluster OIDC Issuer URL
 # Registers an OIDC provider for the EKS cluster, allowing IAM to trust Kubernetes service accounts.
 # The thumbprint_list ensures secure communication.
 # Why is this needed? - This enables Kubernetes ServiceAccounts to authenticate with AWS IAM without needing static credentials.
