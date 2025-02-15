32c540603b6336398b81e547df93c25a4c0f440e
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:52:23 +0400

    Update NAT.tf

--- eks-tf/NAT.tf
+++ eks-tf/NAT.tf
@@ -1,18 +1,21 @@
+# Create an Elastic IP for the NAT Gateway
 resource "aws_eip" "nat" {
-  vpc = true
+  vpc = true # Allocate the EIP in the VPC
 
   tags = {
     Name = "nat"
   }
 }
 
+# Create a NAT Gateway in the public subnet
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_us_west_2a.id
+  allocation_id = aws_eip.nat.id          # Associate the NAT Gateway with the EIP
+  subnet_id     = aws_subnet.public_us_west_2a.id # Place the NAT Gateway in the public subnet
 
   tags = {
     Name = "nat"
   }
 
+  # Ensure the Internet Gateway is created before the NAT Gateway
   depends_on = [aws_internet_gateway.igw]
 }
