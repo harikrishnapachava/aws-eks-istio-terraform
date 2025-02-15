210ceaec95fe5fda18ec9a0c594c3fe0d5a659aa
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:59:25 +0400

    Update EKSOIDC.tf

--- eks-tf/EKSOIDC.tf
+++ eks-tf/EKSOIDC.tf
@@ -1,9 +1,11 @@
+# Retrieve the TLS certificate for the EKS cluster's OIDC provider
 data "tls_certificate" "eks" {
   url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
 }
 
+# Create an OpenID Connect (OIDC) provider for the EKS cluster
 resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"] # Client IDs for the OIDC provider
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint] # Thumbprint of the TLS certificate
+  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer # OIDC issuer URL
 }
