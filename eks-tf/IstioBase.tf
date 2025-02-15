03d7aa215bf789bca017696d4b8c05fa2ca4ecf8
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 16:03:15 +0400

    Update IstioBase.tf

--- eks-tf/IstioBase.tf
+++ eks-tf/IstioBase.tf
@@ -1,15 +1,18 @@
 # helm repo add istio https://istio-release.storage.googleapis.com/charts
 # helm repo update
 # helm install my-istio-base-release -n istio-system --create-namespace istio/base --set global.istioNamespace=istio-system
+
+# Deploy Istio base using Helm
 resource "helm_release" "istio_base" {
   name = "my-istio-base-release"
 
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "base"
-  namespace        = "istio-system"
-  create_namespace = true
-  version          = "1.17.1"
+  repository       = "https://istio-release.storage.googleapis.com/charts" # Helm repository for Istio
+  chart            = "base"                                               # Helm chart for Istio base
+  namespace        = "istio-system"                                       # Namespace for Istio base
+  create_namespace = true                                                 # Create the namespace if it doesn't exist
+  version          = "1.17.1"                                             # Version of Istio base
 
+  # Configuration values for Istio base
   set {
     name  = "global.istioNamespace"
     value = "istio-system"
