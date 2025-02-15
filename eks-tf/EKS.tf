833031780d12bdb7cefea213d446f9b7815fc538
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:56:43 +0400

    Update EKS.tf

--- eks-tf/EKS.tf
+++ eks-tf/EKS.tf
@@ -1,6 +1,8 @@
+# Create an IAM role for the EKS cluster
 resource "aws_iam_role" "demo" {
   name = "eks-istio-servicemesh-demo"
 
+  # Define the trust relationship policy for the role
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -17,16 +19,19 @@ resource "aws_iam_role" "demo" {
 POLICY
 }
 
+# Attach the AmazonEKSClusterPolicy to the IAM role
 resource "aws_iam_role_policy_attachment" "demo_amazon_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.demo.name
 }
 
+# Create the EKS cluster
 resource "aws_eks_cluster" "demo" {
-  name     = "demo"
-  version  = "1.29"
-  role_arn = aws_iam_role.demo.arn
+  name     = "demo" # Name of the EKS cluster
+  version  = "1.29" # Kubernetes version
+  role_arn = aws_iam_role.demo.arn # IAM role for the cluster
 
+  # Configure the VPC for the EKS cluster
   vpc_config {
     subnet_ids = [
       aws_subnet.private_us_west_2a.id,
@@ -36,5 +41,6 @@ resource "aws_eks_cluster" "demo" {
     ]
   }
 
+  # Ensure the IAM role policy is attached before creating the cluster
   depends_on = [aws_iam_role_policy_attachment.demo_amazon_eks_cluster_policy]
 }
