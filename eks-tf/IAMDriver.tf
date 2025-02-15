6b378c8e38ce764b7dbca24a49a6a88e8222ab90
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 18:05:32 +0400

    Update IAMDriver.tf

--- eks-tf/IAMDriver.tf
+++ eks-tf/IAMDriver.tf
@@ -1,4 +1,10 @@
-# Define an IAM policy document for the EBS CSI driver
+# This section sets up an IAM role that allows the AWS Elastic Block Store (EBS) Container Storage Interface (CSI) driver to interact with AWS services.
+
+# Step 1: Define an IAM policy for the EBS CSI driver
+# This policy grants the EBS CSI driver permission to assume the IAM role via OIDC authentication.
+# The condition ensures that only the EBS CSI service account (ebs-csi-controller-sa) in the kube-system namespace can use this role.
+# The principals field links it to the OIDC provider of the EKS cluster.
+
 data "aws_iam_policy_document" "csi" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -17,13 +23,20 @@ data "aws_iam_policy_document" "csi" {
   }
 }
 
-# Create an IAM role for the EBS CSI driver
+#Step 2: Create an IAM role for the EBS CSI driver
+# Creates an IAM role that can be assumed by the EBS CSI driver via OIDC.
+
 resource "aws_iam_role" "eks_ebs_csi_driver" {
   assume_role_policy = data.aws_iam_policy_document.csi.json
   name               = "eks-ebs-csi-driver"
 }
 
-# Attach the AmazonEBSCSIDriverPolicy to the IAM role
+
+#Step 3: Attach the AmazonEBSCSIDriverPolicy to the IAM role
+# Grants the AmazonEBSCSIDriverPolicy, allowing the CSI driver to:
+# Attach/detach EBS volumes.
+# Create/delete EBS volumes dynamically.
+
 resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
   role       = aws_iam_role.eks_ebs_csi_driver.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
