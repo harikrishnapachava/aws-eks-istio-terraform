5d20f1f2d9d91053c822eaf578323fdfd56a021e
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-02-15 15:51:40 +0400

    Update Subnets.tf

--- eks-tf/Subnets.tf
+++ eks-tf/Subnets.tf
@@ -1,8 +1,10 @@
+# Create private subnets in different availability zones
 resource "aws_subnet" "private_us_west_2a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-west-2a"
+  vpc_id            = aws_vpc.main.id       # Associate subnet with the VPC
+  cidr_block        = "10.0.0.0/19"         # CIDR block for the subnet
+  availability_zone = "us-west-2a"          # Availability zone for the subnet
 
+  # Tags for identifying the subnet and enabling internal ELB
   tags = {
     "Name"                            = "private-us-west-2a"
     "kubernetes.io/role/internal-elb" = "1"
@@ -22,11 +24,12 @@ resource "aws_subnet" "private_us_west_2b" {
   }
 }
 
+# Create public subnets in different availability zones
 resource "aws_subnet" "public_us_west_2a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
   availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true # Enable auto-assignment of public IPs
 
   tags = {
     "Name"                       = "public-us-west-2a"
